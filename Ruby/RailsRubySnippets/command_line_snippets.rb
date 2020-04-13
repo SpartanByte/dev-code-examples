c5485b1defe8c6b2411664609fd5f5069113b88e
Brian Wardwell <briwar10@outlook.com>
2020-04-13 11:57:31 -0500

    added command_line_snippets and updated heroku commands

---
+++ Ruby/RailsRubySnippets/command_line_snippets.rb
@@ -0,0 +1,11 @@
+## COMMAND LINE SNIPPETS
+
+## Create a new secret key
+rails secret
+
+## edit credentials file
+rails credentials:edit
+
+## can also have it open up in an editor
+SET EDITOR="some_ide_path"
+credentials:edit
