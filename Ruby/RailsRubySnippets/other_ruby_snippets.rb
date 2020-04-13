3237490f2d318b3172e0c7a7c070051b69dda912
Brian Wardwell <briwar10@outlook.com>
2020-04-13 10:05:09 -0500

    added herkoku_commands.txt

--- Ruby/RailsRubySnippets/other_ruby_snippets.rb
+++ Ruby/RailsRubySnippets/other_ruby_snippets.rb
@@ -2,3 +2,9 @@
 
 ## For adding in custom fonts to assets.rb
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+## For adding in call to the secret key in application.rb
+## this error may happen the first time an app is deployed to heroku
+## in Class Application < Rails::Application..
+config.secret_key_base = ENV["SECRET_KEY_BASE"]
+## then set the key in command line using heroku config:set
