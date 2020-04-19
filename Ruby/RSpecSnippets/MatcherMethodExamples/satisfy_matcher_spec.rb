7d301e6676df767f5fb181101a0f6358df1f55fb
Brian Wardwell <briwar10@outlook.com>
2020-04-19 13:10:55 -0500

    added satify helper spec and compound expecations, more not_to examples

---
+++ Ruby/RSpecSnippets/MatcherMethodExamples/satisfy_matcher_spec.rb
@@ -0,0 +1,19 @@
+# RSPEC SNIPPETS (DOES NOT INCLUDE CAPYBARA)
+# Credit:: https://www.udemy.com/course/testing-ruby-with-rspec/
+
+
+RSpec.describe 'satisfy matcher' do
+    subject{ 'racecar' }
+
+    it 'is a palindrome' do
+        #does not require a string argument but does require a single argument and a comparison that will equal a boolean
+        expect(subject).to satisfy { |value| value == value.reverse }
+    end
+
+    it 'can accept a custom error' do
+        expect(subject).to satisfy('be a palindrome') do |value| 
+            value == value.reverse
+        end
+    end
+
+end
